43ffa3f47d0fa6fb2b6e13802f3f98d8416ebce3
Akayou <akuadane1205@gmail.com>
2022-03-03 21:03:32 +0300

    implemented findMoves for kings as well

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -18,8 +18,11 @@ public class Board {
         for(int r=0;r<this.board.length;r++){
             for(int c=(1-r%2);c<this.board.length;c+=2){ // Makes sure the Pieces are placed on squares where i+j is odd
                 Piece piece = this.board[r][c];
-                if(piece!=null && piece.owner==owner && piece.type==PieceType.PAWN)
-                    this.findMoves(piece.owner,r,c);
+                if(piece!=null && piece.owner==owner){
+                        this.findMoves(piece,r,c);
+                }
+
+
 
             }
         }
@@ -29,50 +32,53 @@ public class Board {
     private void findJumps(PieceOwner owner,int r, int c){}
     private void findKingJumps(PieceOwner owner,int r, int c){}
 
-    private void findMoves(PieceOwner owner,int r, int c){
+    private void findMoves(Piece piece,int r, int c){
         int[][] moveDir;
 
-        if(owner==PieceOwner.PLAYER1)
-            moveDir= new int[][]{{-1,-1},{-1,1}};
-        else
-            moveDir= new int[][]{{1,-1},{1,1}};
-
-            for(int i=0;i< moveDir.length;i++){
-                int newR = r+moveDir[i][0];
-                int newC = c+moveDir[i][1];
-
-                //Check if the new row and column are inside the board
-                if(newR<0 || newR>=this.board.length || newC<0 || newC>=this.board.length)
-                    continue;
-
-                Piece init = this.board[r][c];
-                Piece newPos = this.board[newR][newC];
-
-                // check if the adjacent square is empty
-                if(newPos==null)
-                    this.moveList.add(new int[]{r, c, newR, newC});
-                // check if the adjacent square is occupied by our piece, if so continue
-                else if(newPos.owner==init.owner)
-                    continue;
-                // check if the second adjacent square is occupied or not
-                else{
-                    int nextNewR = newR + moveDir[i][0];
-                    int nextNewC = newC + moveDir[i][1];
-
-                    if(nextNewR<0 || nextNewR>=this.board.length || nextNewC<0 || nextNewC>=this.board.length)
-                        continue;
-                    Piece nextNewPos = this.board[nextNewR][nextNewC];
-
-                    // if the second adjacent square isn't occupied  this is a valid removing move
-                    if(nextNewPos==null)
-                        this.removingMoveList.add(new int[]{r, c, nextNewR, nextNewC});
-
-
-            }
+        if(piece.type==PieceType.PAWN){
+            if(piece.owner==PieceOwner.PLAYER1 )
+                moveDir= new int[][]{{-1,-1},{-1,1}}; // allow player1's pawns to move towards the top of the board only
+            else
+                moveDir= new int[][]{{1,-1},{1,1}};  // allow player2's pawns to move towards the bottom of the board only
         }
+        else
+            moveDir= new int[][]{{1,-1},{1,1},{-1,1},{-1,-1}}; // allow kings to move to every direction
+
+        for(int i=0;i< moveDir.length;i++){
+            int newR = r+moveDir[i][0];
+            int newC = c+moveDir[i][1];
+
+            //Check if the new row and column are inside the board
+            if(newR<0 || newR>=this.board.length || newC<0 || newC>=this.board.length)
+                continue;
+
+            Piece init = this.board[r][c];
+            Piece newPos = this.board[newR][newC];
+
+            // check if the adjacent square is empty
+            if(newPos==null)
+                this.moveList.add(new int[]{r, c, newR, newC});
+            // check if the adjacent square is occupied by our piece, if so continue
+            else if(newPos.owner==init.owner)
+                continue;
+            // check if the second adjacent square is occupied or not
+            else{
+                int nextNewR = newR + moveDir[i][0];
+                int nextNewC = newC + moveDir[i][1];
+
+                if(nextNewR<0 || nextNewR>=this.board.length || nextNewC<0 || nextNewC>=this.board.length)
+                    continue;
+                Piece nextNewPos = this.board[nextNewR][nextNewC];
+
+                // if the second adjacent square isn't occupied  this is a valid removing move
+                if(nextNewPos==null)
+                    this.removingMoveList.add(new int[]{r, c, nextNewR, nextNewC});
+
+        }
+    }
 
     }
-    private void findKingMoves(PieceOwner owner,int r, int c){}
+    //private void findKingMoves(PieceOwner owner,int r, int c){}
     public void makeMove(int[] move){}
     public void makeJump(int[] jump){}
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,14 @@
 package com.company;
-
-import java.util.HashMap;
-
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         Board board = new Board();
         board.display();
-
-        board.findLegalMoves(PieceOwner.PLAYER2);
+        board.findLegalMoves(PieceOwner.PLAYER1);
 
         for(int[] a: board.moveList){
             System.out.printf("%d %d %d %d\n",a[0],a[1],a[2],a[3]);
         }
-       
     }
 }
