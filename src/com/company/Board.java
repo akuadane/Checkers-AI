f7161cbbc63406e14a39983d1a501d94ec4a35d2
Akayou <akuadane1205@gmail.com>
2022-03-03 10:51:36 +0300

    implemented findMoves

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -1,21 +1,78 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Board {
     Piece[][] board = new Piece[8][8];
-    int jumpList[][] = new int[48][12];
-    int moveList[][] = new int[48][12];
+    ArrayList<int[]> jumpList = new ArrayList<>();
+    ArrayList<int[]> moveList = new ArrayList<>();
+    ArrayList<int[]> removingMoveList = new ArrayList<>();
+
 
     public Board(){
         this.resetBoard();
     }
 
     public int[][] findLegalMoves(PieceOwner owner){
+
+        for(int r=0;r<this.board.length;r++){
+            for(int c=(1-r%2);c<this.board.length;c+=2){ // Makes sure the Pieces are placed on squares where i+j is odd
+                Piece piece = this.board[r][c];
+                if(piece!=null && piece.owner==owner && piece.type==PieceType.PAWN)
+                    this.findMoves(piece.owner,r,c);
+
+            }
+        }
+
         return new int[48][12];
     }
-    private void findJumps(PieceOwner owner,int x, int y){}
-    private void findKingJumps(PieceOwner owner,int x, int y){}
-    private void findMoves(PieceOwner owner,int x, int y){}
-    private void findKingMoves(PieceOwner owner,int x, int y){}
+    private void findJumps(PieceOwner owner,int r, int c){}
+    private void findKingJumps(PieceOwner owner,int r, int c){}
+
+    private void findMoves(PieceOwner owner,int r, int c){
+        int[][] moveDir;
+
+        if(owner==PieceOwner.PLAYER1)
+            moveDir= new int[][]{{-1,-1},{-1,1}};
+        else
+            moveDir= new int[][]{{1,-1},{1,1}};
+
+            for(int i=0;i< moveDir.length;i++){
+                int newR = r+moveDir[i][0];
+                int newC = c+moveDir[i][1];
+
+                //Check if the new row and column are inside the board
+                if(newR<0 || newR>=this.board.length || newC<0 || newC>=this.board.length)
+                    continue;
+
+                Piece init = this.board[r][c];
+                Piece newPos = this.board[newR][newC];
+
+                // check if the adjacent square is empty
+                if(newPos==null)
+                    this.moveList.add(new int[]{r, c, newR, newC});
+                // check if the adjacent square is occupied by our piece, if so continue
+                else if(newPos.owner==init.owner)
+                    continue;
+                // check if the second adjacent square is occupied or not
+                else{
+                    int nextNewR = newR + moveDir[i][0];
+                    int nextNewC = newC + moveDir[i][1];
+
+                    if(nextNewR<0 || nextNewR>=this.board.length || nextNewC<0 || nextNewC>=this.board.length)
+                        continue;
+                    Piece nextNewPos = this.board[nextNewR][nextNewC];
+
+                    // if the second adjacent square isn't occupied  this is a valid removing move
+                    if(nextNewPos==null)
+                        this.removingMoveList.add(new int[]{r, c, nextNewR, nextNewC});
+
+
+            }
+        }
+
+    }
+    private void findKingMoves(PieceOwner owner,int r, int c){}
     public void makeMove(int[] move){}
     public void makeJump(int[] jump){}
 
@@ -24,8 +81,8 @@ public class Board {
      * */
     public void resetBoard(){
 
-        for(int i=0;i<=7;i++){
-            for(int j=(1-i%2);j<=7;j+=2){ // Makes sure the Pieces are placed on squares where i+j is odd
+        for(int i=0;i<this.board.length;i++){
+            for(int j=(1-i%2);j<this.board.length;j+=2){ // Makes sure the Pieces are placed on squares where i+j is odd
                 if(i<=2){  //place player two's pawns in their starting place
                     this.board[i][j]= new Piece(PieceType.PAWN,PieceOwner.PLAYER2);
                 }else if(i<=4){ // makes the middle two rows empty
@@ -39,8 +96,8 @@ public class Board {
     }
 
     public void display(){
-        for(int i=0;i<=7;i++){
-            for(int j=0;j<=7;j++){
+        for(int i=0;i<this.board.length;i++){
+            for(int j=0;j<this.board.length;j++){
                 Piece piece = this.board[i][j];
                 char p = ' ';
                 if(piece!=null){
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,19 @@
 package com.company;
 
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         Board board = new Board();
         board.display();
+
+        board.findLegalMoves(PieceOwner.PLAYER2);
+
+        for(int[] a: board.moveList){
+            System.out.printf("%d %d %d %d\n",a[0],a[1],a[2],a[3]);
+        }
+       
     }
 }
