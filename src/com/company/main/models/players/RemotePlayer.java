9e4746f2b833aff999c56e2cea69da83fabf604e
Akayou <akuadane1205@gmail.com>
2022-03-08 19:36:33 +0300

    added RemotePlayer.java

---
+++ src/com/company/main/models/players/RemotePlayer.java
@@ -0,0 +1,17 @@
+package com.company.main.models.players;
+
+import com.company.main.models.Board;
+import com.company.main.models.move.Move;
+import com.company.main.models.piece.PieceOwner;
+
+public class RemotePlayer extends Player{
+    RemotePlayer(String name, PieceOwner myTurn) {
+        super(name, myTurn);
+    }
+
+    @Override
+    public Move makeMove(Board board) {
+        // TODO implement this method
+        return null;
+    }
+}
