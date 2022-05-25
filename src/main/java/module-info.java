2eca351d78afb5fca3dbe6b8849251069a6a9c81
miruts <miruts.hadush@aait.edu.et>
2022-05-25 17:19:14 +0300

    fix borken javafx imports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,10 @@
-module com.company {
+module com.checkers {
     requires javafx.controls;
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
     requires junit;
+    requires javafx.graphics;
 
     opens com.checkers to javafx.fxml;
     exports com.checkers.gui;
