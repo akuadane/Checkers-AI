611b0deff46358d372e17a0f27c736e2ad0ec07b
miruts <miruts.hadush@aait.edu.et>
2022-06-17 15:52:29 +0300

    Add launcher class

---
+++ src/main/java/com/checkers/Launcher.java
@@ -0,0 +1,10 @@
+package com.checkers;
+
+import com.checkers.gui.ChoosePlayer;
+
+public class Launcher {
+    public static void main(String[] args) {
+        ChoosePlayer.main(args);
+    }
+}
+
